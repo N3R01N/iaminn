2ea3cee6411deab888095b338bfa3bdd22853fa1
Matthias <matthiasschermann@gmail.com>
2013-11-21 17:46:10 +0100

    test

--- test/GeoTagging/src/com/testrunns/geotagging/CameraActivity.java
+++ test/GeoTagging/src/com/testrunns/geotagging/CameraActivity.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
 public class CameraActivity extends Activity {
+	
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	
+	Bitmap mImageBitmap;
+	ImageView mImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_camera_activity);
+		
+		mImageView = (ImageView) findViewById(R.id.imageView1);
 	}
 
 	@Override
@@ -23,15 +33,27 @@ public class CameraActivity extends Activity {
 		return true;
 	}
 	
-	public void takePicture(View view){
+	public void onTakePictureButtonClick(View view){
 		Log.d("buttonClick","Should take pic now!");
 		//CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-		dispatchTakePictureIntent(100);
+		takePicture(CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	private void takePicture(int requestCode) {
+		Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(takePicIntent, requestCode);	
 	}
 	
-	private void dispatchTakePictureIntent(int actionCode) {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    startActivityForResult(takePictureIntent, actionCode);
+	private void displayImageThumbnail(Intent intent){
+		Bundle extras = intent.getExtras();
+		mImageBitmap = (Bitmap) extras.get("data");
+		mImageView.setImageBitmap(mImageBitmap);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		displayImageThumbnail(data);
 	}
 
 }
